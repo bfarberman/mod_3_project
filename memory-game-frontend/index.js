df9e5f003724d09c634107855a80d2e9bfddf81d
Sasha <apestruy@gmail.com>
2020-02-11 13:34:27 -0500

    makes grid

--- memory-game-frontend/index.js
+++ memory-game-frontend/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-
+    let cardFlip = 0
     const containerDiv = document.getElementsByClassName("grid-container")[0]
 
     fetch("http://localhost:3000/api/v1/cards")
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function(){
             array.sort(() => Math.random() - 0.5);
           }
           
-          shuffle(doubledCards);
+        shuffle(doubledCards);
 
         doubledCards.forEach(card => {
             const littleDiv = document.createElement('div');
@@ -20,13 +20,39 @@ document.addEventListener('DOMContentLoaded', function(){
             littleDiv.setAttribute('class', 'grid-item');
             littleDiv.dataset.id = card.id;
             image.setAttribute('class', 'card-image');
-            image.src = card.imageUrl
+            image.src = "https://image.freepik.com/free-vector/vector-creativity-concept-brain-light-bulb_106427-92.jpg"
             littleDiv.appendChild(image)
             containerDiv.appendChild(littleDiv)
+                
+            littleDiv.addEventListener("click", function(e){
+                const array = [] 
+                cardFlip ++
+                if (cardFlip <= 2) {
+                    e.target.src = card.imageUrl
+                    array.push(card)
+                }     
+                // cardFlip = 0
+
+                // if (cardFlip <= 2)
+                // e.target.src = card.imageUrl
+
+
+
+
+            })
+
         })
     })
     
-       
     
-
+    
 })
+
+// image.src = card.imageUrl
+
+// containerDiv.addEventListener("click", function(e){
+//     e.target.src = card.imageUrl
+// })
+// containerDiv.addEventListener("mouseleave", function(e){
+//     e.target.src = 
+// })
