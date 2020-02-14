a56d933cb2871fbdeb15e9477862b922fde91bfd
Sasha <apestruy@gmail.com>
2020-02-14 14:23:44 -0500

    FINISHED

--- memory-game-frontend/index.js
+++ memory-game-frontend/index.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 cardFlip = 0
                 cardArray.length = 0
                 scoreKeeper()
-            }, 1000);
+            }, 500);
         } else {
             setTimeout(function () {
                 cardArray[0].src = "https://image.freepik.com/free-vector/vector-creativity-concept-brain-light-bulb_106427-92.jpg"
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 cardFlip = 0
                 cardArray.length = 0
-            }, 1000);
+            }, 500);
         }
     }
 
@@ -80,6 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let timer = setInterval(decrementCounter, 1000)
 
     function decrementCounter() {
+        finishEarly()
         let currentSec = counter.innerText
         let numSec = parseInt(currentSec)
         numSec = numSec - 1
@@ -99,28 +100,50 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let popUp = document.getElementById('light')
 
+    function finishEarly() {
+        const score = document.getElementById("score")
+        if (parseInt(score.innerText.split(" ")[1]) === 100) {
+            gameOver()
+        }
+    }
+
     function gameOver() {
         clearInterval(timer)
         popUp.style.display = 'block';
         let currentScore = score.innerText.split(" ")[1]
-        let numScore = parseInt(currentScore)        
+        let numScore = parseInt(currentScore)       
         
-        popUp.innerHTML = `
-        <button id="close">X</button>
-        <h2><strong>Game Over! <br>
-        Your score was ${numScore}. </h2><br>
+        if (parseInt(score.innerText.split(" ")[1]) === 100) {
+            popUp.innerHTML = `
+            <button id="close">X</button>
+            <h2><strong>You Win! <br>
+            Your score was ${numScore}. </h2><br>
+            
+            <form class="score" style="">
+            <h3>Enter your name:</h3>
+            <input id="username" type="text" name="username" value="" class="input-text">
+            <br>
+            <input id="submit" type="submit" name="submit" value="Submit" class="submit">
+            </form>`
+        } else {
+
+            popUp.innerHTML = `
+            <button id="close">X</button>
+            <h2><strong>Game Over! <br>
+            Your score was ${numScore}. </h2><br>
+            
+            <form class="score" style="">
+            <h3>Enter your name:</h3>
+            <input id="username" type="text" name="username" value="" class="input-text">
+            <br>
+            <input id="submit" type="submit" name="submit" value="Submit" class="submit">
+            </form>`
+        }
         
-        <form class="score" style="">
-        <h3>Enter your name:</h3>
-        <input id="username" type="text" name="username" value="" class="input-text">
-        <br>
-        <input id="submit" type="submit" name="submit" value="Submit" class="submit">
-        </form>`
         
         const xButton = document.getElementById("close")
 
         xButton.addEventListener("click", (e)=>{
-            console.log("hi")
             popUp.style.display = 'none'
         })
 
@@ -129,7 +152,7 @@ document.addEventListener('DOMContentLoaded', function () {
             e.preventDefault()
             let name = e.target.username.value
             let score = parseInt(e.target.parentElement.parentElement.children[1].innerText.split(' ')[4])
-            cardSetId = parseInt(e.target.parentElement.parentElement.parentElement.children[4].dataset.id)
+            cardSetId = parseInt(document.getElementsByClassName("grid-container")[0].dataset.id)
                 fetch("http://localhost:3000/api/v1/games", {
                     method: "POST",
                     headers: {
@@ -165,12 +188,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 hideButton.addEventListener("click", function(e){
                     leaderboardPopUp.style.display = 'none'
+                    leaderDiv.innerHTML = ""
                 })
 
-                console.log(games)
                 games.forEach(game => {
                     const li = document.createElement('li');
-                    li.innerHTML = `Name: ${game.username} &nbsp &nbsp &nbsp Score: (${game.score})`
+                    li.innerHTML = `Score: (${game.score}) &nbsp &nbsp &nbsp Name: ${game.username}`
                     const deleteButton = document.createElement('button');
                     deleteButton.setAttribute("id", "delete")
                     deleteButton.innerText = "Delete"
@@ -185,10 +208,6 @@ document.addEventListener('DOMContentLoaded', function () {
                             leaderBoard()
 
                         })
-                        // .then(resp=>resp.json())
-                        // .then(data => console.log(data))
-
-
                     })
                     li.appendChild(deleteButton)
                     ol.appendChild(li)
@@ -198,8 +217,6 @@ document.addEventListener('DOMContentLoaded', function () {
             })
     }
 
-
-
     topScores = document.querySelector("a")
 
     topScores.addEventListener("click", function(e){
@@ -207,30 +224,24 @@ document.addEventListener('DOMContentLoaded', function () {
         leaderBoard()
     })
 
+    function restart() {
+        clearInterval(timer)
+        counter.innerText = 60
+        timer = setInterval(decrementCounter, 1000)
+        startGame()
+    }
 
+    restartButton = document.getElementById("restart")
+    restartButton.addEventListener('click', function (event) {
+            restart() 
+        }
+    )
 
 })
 
-        // function restart() {
-        //     popUp.style.display = "none"
-        //     counter.innerText = 22
-        //     timer = setInterval(decrementCounter, 1000)
-        //     startGame()
-        // }
 
 
-
-        // const topScores = document.getElementById('leaderboard')
-
-
-
-    // document.addEventListener('keydown', function (event) {
-    //     if (event.target.key = "r") {
-    //         if (parseInt(counter.innerText) < 1) {
-    //             restart() 
-    //         }
-    //     }
-    // })
+        
 
 
 
@@ -251,22 +262,4 @@ document.addEventListener('DOMContentLoaded', function () {
     //     })
     // }
 
-    // Would you like to play again? <br>
-    // Press "R" to restart.</strong>
 
-            // .then(function (response) {
-            //     return response.json();
-            // }).then(function (game) {
-            //     const ul = addButton.parentNode.querySelector("ul")
-            //     if (ul.children.length < 6) { 
-            //         const li = document.createElement('li');
-            //         li.innerText = `${pokemon.nickname} (${pokemon.species})`
-            //         const releaseButton = document.createElement('button');
-            //         releaseButton.setAttribute('class', 'release');
-            //         releaseButton.innerText = "Release";
-            //         releaseButton.dataset.id = pokemon.id;
-            //         releasePokemon(releaseButton)
-            //         li.appendChild(releaseButton)
-            //         ul.appendChild(li)
-            //     } else {alert(pokemon.error) }
-            // });
